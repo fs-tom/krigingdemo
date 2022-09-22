6578f8c504afd93ab403c62656d0df42157ed18d
tom <tom@that-place>
2022-09-22 16:53:56 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject smiletest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [spork "0.2.1.3-SNAPSHOT"]
+                 [org.clojars.haifengl/smile "2.5.3"]
+                 [com.github.haifengl/smile-interpolation "2.5.3"]
+                 [com.github.haifengl/smile-plot "2.5.3"]
+                 [com.github.haifengl/smile-mkl "2.5.3"]])
